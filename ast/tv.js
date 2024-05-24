d93f79e93c62f28d6cacc59ae38591b534160b85
tirsod <tdanielv2000@gmail.com>
2024-05-24 19:18:13 -0400

    LMAO

--- ast/tv.js
+++ ast/tv.js
@@ -1,26 +1,16 @@
 
 selectedMessage = "";
-selectedFlavor = "";
-
 printedMessage = "";
-printedFlavor = "";
-// test
 var welcome;
-var flavor;
 
 window.onload = function(){
-  welcomeMessages = ["welcome."]
-  flavorTexts = ["Can you come back later? We're working on something."];
+  welcomeMessages = ["hi.", "take a seat", "today's programming is...", "welcome", "who's your friend?", "who's that behind you?", "should go on a walk soon...", "hmm... did i drink enough water today?", "make yourself at home", "enjoying yourself?", "you are not alone", "want some coffee?", "-6410"];
 
-  welcome = document.getElementById("welcome");
-  flavor = document.getElementById("welcome2");
   n = Math.floor(Math.random()*welcomeMessages.length);
+  welcome = document.getElementById("welcome");
   selectedMessage = welcomeMessages[n];
-  n = Math.floor(Math.random()*flavorTexts.length);
-  selectedFlavor = flavorTexts[n];
 
   setTimeout(addLetter, 50);
-  setTimeout(addLetter2, 1000);
 }
 
 addLetter = function(){
@@ -30,10 +20,15 @@ addLetter = function(){
     setTimeout(addLetter, 50);
   }
 }
-addLetter2 = function(){
-  if (printedFlavor.length < selectedFlavor.length){
-    printedFlavor += selectedFlavor[printedFlavor.length];
-    flavor.innerHTML = printedFlavor;
-    setTimeout(addLetter2, 50);
-  }
-}
+
+
+//Give me that video bro
+/*
+var channelID = "UCgctJZ2fW1bXb6FRj2kGzXw";
+var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+   var link = data.items[0].link;
+   var id = link.substr(link.indexOf("=")+1);
+$("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+});
+*/
