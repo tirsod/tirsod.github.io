80fdd22a1fac3b0458facc7163ef6141dd486edf
tirsod <tdanielv2000@gmail.com>
2025-03-13 22:36:56 -0400

    6410 TV IS NOT A REAL TV CHANNEL

--- ast/tv.js
+++ ast/tv.js
@@ -5,21 +5,22 @@ var welcome;
 
 window.onload = function(){
   welcomeMessages = ["hi.", "take a seat", "today's programming is...", "welcome",
-    "who's your friend?", "who's that behind you?", "should go on a walk soon...",
+    "who's your friend?", "who's that behind you?", "you should go for a walk...",
     "hmm... did i drink enough water today?", "make yourself at home", "enjoying yourself?",
-    "you are not alone", "want some coffee?", "-6410",
-    "been a while since i last saw you.\nhow's it been?", "someone, somewhere, is having a blast.",
+    "you are not alone...", "too many realities.", "-6410",
+    "it's been a while.\nhow have you been?", "someone, somewhere... is watching us.",
     "can't organize these thoughts well enough.", "someone's going to have to fix that.",
     "getting interference...", "are you also getting those visions?", "everyone's talking about you.",
-    "the sun lives to light another day.", "i know. too many choices for one person.",
-    "i don't blame you.", "i would have done it too.", "simply not enough time.",
-    "the name isn't important.", "you don't need to know the name\nto know the person.",
-    "show them who you are", "don't be afraid to break out of convention.",
+    "the sun lives to see another day.", "i know. too many paths for one person.",
+    "i don't blame you.", "i make that mistake a lot.", "simply not enough time.",
+    "there might not be enough time.", "you don't need to know the name..",
+    "show them who you are", "shh... it can hear us.",
     "hm...? you brought me something?", "bring me something new.", "don't worry about me.",
-    "that's my type of music.", "got lost?", "it's normal to get lost around these parts.",
+    "that's my type of music.", "you get lost?", "it's normal to get lost around these parts.",
     "you'll find your way.", "well, look who it is.",
     "the static... it's getting louder...", "too many layers...",
-    "someone needs to get away from that dial...", "you're back. welcome back."];
+    "someone needs to get away from that dial...", "you're back. welcome back.",
+    "someone looks... lost..."];
 
   n = Math.floor(Math.random()*welcomeMessages.length);
   welcome = document.getElementById("welcome");
